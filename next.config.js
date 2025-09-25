93cc43e0beb546d7689f39f36e3ea192bd535b5f
kaifutsi <ekaterina_94_1994@icloud.com>
2025-09-25 12:40:03 +0300

    chore: custom domain (CNAME), remove basePath/assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repo = 'maa-finn';
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : ''
-  }
 };
-
 module.exports = nextConfig;
